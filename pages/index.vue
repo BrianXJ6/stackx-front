de378b64550d87d43865df011bca617ab39dbc79
Brian Barros <brian.ferreira@pie.tax>
2024-05-26 01:34:30 -0300

    Initial installation

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container my-5">
+    <h1>Hello World</h1>
+    <hr>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam eius exercitationem repellat. Odio error, accusamus nisi blanditiis unde hic ipsam, laudantium officiis incidunt possimus sit repellendus dolor velit nemo quo?
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
